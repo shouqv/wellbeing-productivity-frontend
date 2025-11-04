c5f8d3441046491694d4bca18fa53fbe7e1dd1ad
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-11-04 13:36:59 +0300

    changing the nav bar so that user can know which page they are in

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router'
 import LogOutButton from '../Auth/LogOutButton'
 
@@ -8,7 +9,15 @@ import goals from '../../assets/dark_goals.png'
 import tasks from '../../assets/dark_tasks.png'
 import checkin from '../../assets/dark_checkin.png'
 
+
+import dashboardActive from '../../assets/dashboard.png'
+import visionboardActive from '../../assets/visionboard.png'
+import goalsActive from '../../assets/goals.png'
+import tasksActive from '../../assets/tasks.png'
+import checkinActive from '../../assets/checkin.png'
+
 function NavBar({ user, setUser }) {
+  const [activeNav , setActiveNav] = useState('dashboard')
   return (
     <nav>
 
@@ -18,11 +27,24 @@ function NavBar({ user, setUser }) {
           <>
 
             <div className='navbar-elements'>
-            <Link to={'/dashboard'}><img src={dashboard} alt="Dashboard" width={24} /></Link>
-            <Link to={'/visionboard'}><img src={visionboard} alt="Dashboard" width={24} /></Link>
-            <Link to={'/goals'}><img src={goals} alt="Dashboard" width={24} /></Link>
-            <Link to={'/tasks'}><img src={tasks} alt="Dashboard" width={24} /></Link>
-            <Link to={'/mood'}><img src={checkin} alt="Dashboard" width={24} /></Link>
+            <Link to={'/dashboard'} onClick={()=> setActiveNav('dashboard')} className={activeNav === 'dashboard'? 'activeNavBar' : ''}>
+            <img src={activeNav === 'dashboard'? dashboardActive : dashboard} alt="Dashboard" width={26} />
+            </Link>
+
+            <Link to={'/visionboard'} onClick={()=> setActiveNav('visionboard')} className={activeNav === 'visionboard'? 'activeNavBar' : ''}>
+            <img src={activeNav === 'visionboard'? visionboardActive : visionboard} alt="vision" width={26}  /></Link>
+
+
+
+            <Link to={'/goals'} onClick={()=> setActiveNav('goals')} className={activeNav === 'goals'? 'activeNavBar' : ''}>
+            <img src={activeNav === 'goals'? goalsActive : goals} alt="goals" width={26}   /></Link>
+
+            <Link to={'/tasks'} onClick={()=> setActiveNav('tasks')} className={activeNav === 'tasks'? 'activeNavBar' : ''}>
+            <img src={activeNav === 'tasks'? tasksActive : tasks}  alt="tasks" width={26}  /></Link>
+
+            
+            <Link to={'/mood'} onClick={()=> setActiveNav('checkin')} className={activeNav === 'checkin'? 'activeNavBar' : ''}>
+            <img src={activeNav === 'checkin'? checkinActive : checkin}  alt="checkin" width={26}  /></Link>
              
             <LogOutButton setUser={setUser} />
             </div>
