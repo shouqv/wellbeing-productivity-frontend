f6653d0c013330552d36c8df2ce9544ec5a367e0
Shouq Almutairi <shouqalmutaiiri8@gmail.com>
2025-11-05 12:25:58 +0300

    fixing the active nav bar if user reload the page, the icon highlited will stay the same

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import {useState} from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link, useLocation } from 'react-router'
 import LogOutButton from '../Auth/LogOutButton'
 
 import dashboard from '../../assets/dark_dashboard.png'
@@ -9,7 +8,6 @@ import goals from '../../assets/dark_goals.png'
 import tasks from '../../assets/dark_tasks.png'
 import checkin from '../../assets/dark_checkin.png'
 
-
 import dashboardActive from '../../assets/dashboard.png'
 import visionboardActive from '../../assets/visionboard.png'
 import goalsActive from '../../assets/goals.png'
@@ -17,48 +15,39 @@ import tasksActive from '../../assets/tasks.png'
 import checkinActive from '../../assets/checkin.png'
 
 function NavBar({ user, setUser }) {
-  const [activeNav , setActiveNav] = useState('dashboard')
+  const location = useLocation()
+  const currentPath = location.pathname
+
+
   return (
     <nav>
+      {user && (
+        <div className="navbar-elements">
+          <Link to="/dashboard" className={currentPath.startsWith('/dashboard') ? 'activeNavBar' : ''}>
+            <img src={currentPath.startsWith('/dashboard') ? dashboardActive : dashboard} alt="Dashboard" width={26} />
+          </Link>
 
-      {
-        user
-          ?
-          <>
+          <Link to="/visionboard" className={currentPath.startsWith('/visionboard') ? 'activeNavBar' : ''}>
+            <img src={currentPath.startsWith('/visionboard') ? visionboardActive : visionboard} alt="Vision Board" width={26} />
+          </Link>
 
-            <div className='navbar-elements'>
-            <Link to={'/dashboard'} onClick={()=> setActiveNav('dashboard')} className={activeNav === 'dashboard'? 'activeNavBar' : ''}>
-            <img src={activeNav === 'dashboard'? dashboardActive : dashboard} alt="Dashboard" width={26} />
-            </Link>
+          <Link to="/goals" className={currentPath.startsWith('/goals') ? 'activeNavBar' : ''}>
+            <img src={currentPath.startsWith('/goals') ? goalsActive : goals} alt="Goals" width={26} />
+          </Link>
 
-            <Link to={'/visionboard'} onClick={()=> setActiveNav('visionboard')} className={activeNav === 'visionboard'? 'activeNavBar' : ''}>
-            <img src={activeNav === 'visionboard'? visionboardActive : visionboard} alt="vision" width={26}  /></Link>
+          <Link to="/tasks" className={currentPath.startsWith('/tasks') ? 'activeNavBar' : ''}>
+            <img src={currentPath.startsWith('/tasks') ? tasksActive : tasks} alt="Tasks" width={26} />
+          </Link>
 
+          <Link to="/mood" className={currentPath.startsWith('/mood') ? 'activeNavBar' : ''}>
+            <img src={currentPath.startsWith('/mood') ? checkinActive : checkin} alt="Mood" width={26} />
+          </Link>
 
-
-            <Link to={'/goals'} onClick={()=> setActiveNav('goals')} className={activeNav === 'goals'? 'activeNavBar' : ''}>
-            <img src={activeNav === 'goals'? goalsActive : goals} alt="goals" width={26}   /></Link>
-
-            <Link to={'/tasks'} onClick={()=> setActiveNav('tasks')} className={activeNav === 'tasks'? 'activeNavBar' : ''}>
-            <img src={activeNav === 'tasks'? tasksActive : tasks}  alt="tasks" width={26}  /></Link>
-
-            
-            <Link to={'/mood'} onClick={()=> setActiveNav('checkin')} className={activeNav === 'checkin'? 'activeNavBar' : ''}>
-            <img src={activeNav === 'checkin'? checkinActive : checkin}  alt="checkin" width={26}  /></Link>
-            
-            <LogOutButton setUser={setUser} />
-            </div>
-          
-
-          </>
-
-          :
-          <>
-          </>
-      }
-
+          <LogOutButton setUser={setUser} />
+        </div>
+      )}
     </nav>
   )
 }
 
-export default NavBar
+export default NavBar;
